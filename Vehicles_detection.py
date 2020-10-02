9cef428c355860cccc9987ec5d0e1cbacd3710a4
RaghavSharma905 <72238870+RaghavSharma905@users.noreply.github.com>
2020-10-02 19:18:16 +0530

    Update Vehicles_detection.py

--- Vehicles_detection.py
+++ Vehicles_detection.py
@@ -1,4 +1,4 @@
-import cv2
+import cv2 #here cv2 is a computer vision module.
 
 # capture frames from a video
 cap = cv2.VideoCapture(r'D:\Prog\OpenCV\Car Detection\New-GithubTest-master\carr.mp4')
@@ -8,7 +8,7 @@ car_cascade = cv2.CascadeClassifier(r'D:\Prog\OpenCV\Car Detection\New-GithubTes
 
 # loop runs if capturing has been initialized.
 while True:
-    ret, frames = cap.read()
+    ret, frames = cap.read() # ret gives us true or false(boolen) value.
     gray = cv2.cvtColor(frames, cv2.COLOR_BGR2GRAY)
     cars = car_cascade.detectMultiScale(gray, 1.1, 1)
     for (x, y, w, h) in cars:
